3391fb274604027af0a95f45c7b590f38cbf04e3
din.amen <din.amen@whitecloak.com>
2019-10-14 08:30:55 +0800

    minor revision

--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -12,32 +12,41 @@ const movieRouter = express.Router();
 
 movieRouter.use(bodyParser.json());
 
-
-movieRouter.get("/movieList", authenticate.jwtCheck, (req, res, next)=>{
+// Display All Movies
+movieRouter.get("/movieList", (req, res)=>{
   const resPerPage = 10; // results per page
-  const page = req.query.page || 1 // Page 
+  const page = parseInt(req.query.page) || 1 // Page 
   mongoose.connection.db
   .collection("movieDetails")
   .find({},  { projection: { _id: 1, title:1, actors: 1, imdb:1, poster: 1, plot: 1 }})
-  .sort({"imdb.rating":-1})
+  // .sort({"imdb.rating":-1})
   .skip((resPerPage * page) - resPerPage)
   .limit(resPerPage)
   .toArray()
-  .then((movies) => {
+  .then(movies => {
     movies.forEach((arrayItem)=>{
-    if(arrayItem.poster !=null){
-      var poster = arrayItem.poster
-      var posterLink = poster.split("/")
-      var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
-      arrayItem.poster = image
-    }})
-    res.setHeader("Content-Type", "application/json");
-    res.json(movies);
-  }, (err) => next(err))
-.catch((err) => next(err)); 
-});
+      if(arrayItem.poster !=null){
+        var poster = arrayItem.poster
+        var posterLink = poster.split("/")
+        var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
+        arrayItem.poster = image
+      }})
+    if (movies.length === 0){
+        res.json("No more results ");
+     } else{
+       mongoose.connection.db
+       .collection("movieDetails")
+       .find({})
+       .count({})
+            .then(numberOfResult => {
+                var pages = Math.ceil(numberOfResult/resPerPage)
+                res.json({currentPage: page, results: numberOfResult, totalPage: pages, movies: movies});
+            })
+  }}, (err) => next(err))
+    .catch((err) => next(err)); 
+  });
 
-movieRouter.get("/movie/:_id", authenticate.jwtCheck, (req, res, next)=>{
+movieRouter.get("/movie/:_id", (req, res, next)=>{
   mongoose.connection.db
   .collection("movieDetails")
   .findOne({_id: new ObjectId(req.params._id)})
@@ -53,28 +62,38 @@ movieRouter.get("/movie/:_id", authenticate.jwtCheck, (req, res, next)=>{
 .catch((err) => next(err)); 
 });
 
-movieRouter.get("/title", authenticate.jwtCheck, (req, res, next)=>{
-  const resPerPage = 1; // results per page
-  const page = req.query.page || 1 // Page 
+movieRouter.get("/title", authenticate.jwtCheck,(req, res, next)=>{
+  const resPerPage = 10; // results per page
+  const page = parseInt(req.query.page) || 1 // Page 
     mongoose.connection.db
     .collection("movieDetails")
-    .find(({title: new RegExp(req.query.title.toLowerCase(),'ig')}))
+    .find(({title: new RegExp(req.query.title,'ig')}))
     .skip((resPerPage * page) - resPerPage)
     .limit(resPerPage)
     .toArray()
-    .then((movies) => {
+    .then(movies => {
       movies.forEach((arrayItem)=>{
         if(arrayItem.poster !=null){
           var poster = arrayItem.poster
           var posterLink = poster.split("/")
           var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
-         arrayItem.poster = image;
-      }})
-        res.json(movies);
-    }, (err) => next(err))
-    .catch((err) => next(err)); 
+          arrayItem.poster = image
+        }})
+      if (movies.length === 0){
+          res.json("No more results ");
+       } else{
+         mongoose.connection.db
+         .collection("movieDetails")
+         .find(({title: new RegExp(req.query.title,'ig')}))
+         .count({})
+              .then(numberOfResult => {
+                  var pages = Math.ceil(numberOfResult/resPerPage)
+                  res.json({currentPage: page, result: numberOfResult, totalPage: pages, movies: movies});
+              });
+    }}, (err) => next(err))
+      .catch((err) => next(err)); 
 });
-
+  
 
 movieRouter.get("/movie/:_id/countries", authenticate.jwtCheck,  (req, res, next)=>{
 mongoose.connection.db
@@ -101,141 +120,235 @@ mongoose.connection.db
 });
 
 
- movieRouter.get("/writers", authenticate.jwtCheck, (req, res, next)=>{
-  const resPerPage = 1; // results per page
-  const page = req.query.page || 1 // Page 
+ movieRouter.get("/writers",  authenticate.jwtCheck, (req, res, next)=>{
+  const resPerPage = 10; // results per page
+  const page = parseInt(req.query.page )|| 1 // Page 
   mongoose.connection.db               
   .collection("movieDetails")
   .find(({writers: new RegExp(req.query.writer.toLowerCase(),'ig')}), {projection: {_id:0, title:1, poster:1, actors:1, plot:1}})
   .skip((resPerPage * page) - resPerPage)
   .limit(resPerPage)
   .toArray()
-  .then((movies) => {
+  .then(movies => {
     movies.forEach((arrayItem)=>{
       if(arrayItem.poster !=null){
         var poster = arrayItem.poster
         var posterLink = poster.split("/")
         var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
-       arrayItem.poster = image;
-    }})
-      res.json(movies);
-  }, (err) => next(err))
-  .catch((err) => next(err)); 
+        arrayItem.poster = image
+      }})
+    if (movies.length === 0){
+        res.json("No more results ");
+     } else{
+       mongoose.connection.db
+       .collection("movieDetails")
+       .find(({writers: new RegExp(req.query.writer.toLowerCase(),'ig')}), {projection: {_id:0, title:1, poster:1, actors:1, plot:1}})
+       .count({})
+            .then(numberOfResult => {
+                var pages = Math.ceil(numberOfResult/resPerPage)
+                res.json({currentPage: page, result: numberOfResult, totalPage: pages, movies: movies});
+            });
+  }}, (err) => next(err))
+    .catch((err) => next(err)); 
 });
 
-
-movieRouter.get("/searchBy", authenticate.jwtCheck,  (req, res, next)=>{
-  const resPerPage = 1; // results per page
-  const page = req.query.page || 1 // Page 
- if(req.query.all && req.query.page){
+movieRouter.get("/searchBy",  (req, res, next)=>{
+  const resPerPage = 10; // results per page
+  const page = parseInt(req.query.page) || 1 // Page 
+ if(req.query.all){
     mongoose.connection.db
     .collection("movieDetails")
-    .find({$or: [{actors: new RegExp(req.query.all.toLowerCase(), 'ig')}, 
-                  {title: new RegExp(req.query.all.toLowerCase(), 'ig')},
-                 {plot: new RegExp(req.query.all.toLowerCase(), 'ig')}]}, { projection: { _id: 0, title:1, actors:1, plot:1, poster:1 }})
+    .find({$or: [{actors: new RegExp(req.query.all, 'ig')}, 
+                  {title: new RegExp(req.query.all, 'ig')},
+                 {plot: new RegExp(req.query.all,'ig')}]}, { projection: { _id: 0, title:1, actors:1, plot:1, poster:1 }})
     .skip((resPerPage * page) - resPerPage)
     .limit(resPerPage)
     .toArray()
-    .then((movies) => {
-          movies.forEach((arrayItem)=>{
-              if(arrayItem.poster !=null){
-                  var poster = arrayItem.poster
-                  var posterLink = poster.split("/")
-                  var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
-                      arrayItem.poster = image;
-              }})
-                res.json(movies);
-          }, (err) => next(err))
+    .then(movies => {
+      movies.forEach((arrayItem)=>{
+        if(arrayItem.poster !=null){
+          var poster = arrayItem.poster
+          var posterLink = poster.split("/")
+          var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
+          arrayItem.poster = image
+        }})
+      if (movies.length === 0){
+          res.json("No more results ");
+       } else{
+         mongoose.connection.db
+         .collection("movieDetails")
+         .find({$or: [{actors: new RegExp(req.query.all, 'ig')}, 
+                      {title: new RegExp(req.query.all, 'ig')},
+                      {plot: new RegExp(req.query.all, 'ig')}]}, { projection: { _id: 0, title:1, actors:1, plot:1, poster:1 }})
+          .count({})
+              .then(numberOfResult => {
+                  var pages = Math.ceil(numberOfResult/resPerPage)
+                  res.json({currentPage: page, result: numberOfResult, totalPage: pages, movies: movies});
+              });
+    }}, (err) => next(err))
   }
                
-   else if(req.query.actor && req.query.page){
+   else if(req.query.actor){
     mongoose.connection.db
       .collection("movieDetails")
-      .find({actors: new RegExp(req.query.actor.toLowerCase(), 'ig')}, { projection: { _id: 0, title:1, actors:1, plot:1, poster:1 }})
+      .find({actors: new RegExp(req.query.actor, 'ig')}, { projection: { _id: 0, title:1, actors:1, plot:1, poster:1 }})
       .skip((resPerPage * page) - resPerPage)
       .limit(resPerPage)
       .toArray()
-      .then((movies) => {
-            movies.forEach((arrayItem)=>{
-                if(arrayItem.poster !=null){
-                    var poster = arrayItem.poster
-                    var posterLink = poster.split("/")
-                    var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
-                        arrayItem.poster = image;
-                }})
-                  res.json(movies);
-            }, (err) => next(err))
+      .then(movies => {
+        movies.forEach((arrayItem)=>{
+          if(arrayItem.poster !=null){
+            var poster = arrayItem.poster
+            var posterLink = poster.split("/")
+            var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
+            arrayItem.poster = image
+          }})
+        if (movies.length === 0){
+            res.json("No more results ");
+         } else{
+           mongoose.connection.db
+           .collection("movieDetails")
+           .find({actors: new RegExp(req.query.actor, 'ig')}, { projection: { _id: 0, title:1, actors:1, plot:1, poster:1 }})
+           .count({})
+                .then(numberOfResult => {
+                    var pages = Math.ceil(numberOfResult/resPerPage)
+                    res.json({currentPage: page, result: numberOfResult, totalPage: pages, movies: movies});
+                });
+      }}, (err) => next(err))
     }
-    else if(req.query.plot && req.query.page){
+    else if(req.query.plot){
       mongoose.connection.db
       .collection("movieDetails")
-      .find({plot: new RegExp(req.query.plot.toLowerCase(), 'ig')}, { projection: { _id: 0, title:1, actors:1, plot:1, poster:1 }})
+      .find({plot: new RegExp(req.query.plot,'ig')}, { projection: { _id: 0, title:1, actors:1, plot:1, poster:1 }})
       .skip((resPerPage * page) - resPerPage)
       .limit(resPerPage)
       .toArray()
-      .then((movies) => {
-            movies.forEach((arrayItem)=>{
-                if(arrayItem.poster !=null){
-                    var poster = arrayItem.poster
-                    var posterLink = poster.split("/")
-                    var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
-                        arrayItem.poster = image;
-                }})
-                  res.json(movies);
-            }, (err) => next(err))
+      .then(movies => {
+        movies.forEach((arrayItem)=>{
+          if(arrayItem.poster !=null){
+            var poster = arrayItem.poster
+            var posterLink = poster.split("/")
+            var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
+            arrayItem.poster = image
+          }})
+        if (movies.length === 0){
+            res.json("No more results ");
+         } else{
+           mongoose.connection.db
+           .collection("movieDetails")
+           .find({plot: new RegExp(req.query.plot, 'ig')}, { projection: { _id: 0, title:1, actors:1, plot:1, poster:1 }})
+           .count({})
+                .then(numberOfResult => {
+                    var pages = Math.ceil(numberOfResult/resPerPage)
+                    res.json({currentPage: page, result: numberOfResult, totalPage: pages, movies: movies});
+                });
+      }}, (err) => next(err))
     }
-  else if(req.query.title && req.query.page){
+    else if(req.query.title){
     mongoose.connection.db
     .collection("movieDetails")
-    .find(({title: new RegExp(req.query.title.toLowerCase(),'ig')}), { projection: { _id: 0, title:1, actors:1, plot:1, poster:1 }})
+    .find(({title: new RegExp(req.query.title,'ig')}), { projection: { _id: 0, title:1, actors:1, plot:1, poster:1 }})
     .skip((resPerPage * page) - resPerPage)
     .limit(resPerPage)
     .toArray()
-    .then((movies) => {
-          movies.forEach((arrayItem)=>{
-              if(arrayItem.poster !=null){
-                  var poster = arrayItem.poster
-                  var posterLink = poster.split("/")
-                  var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
-                      arrayItem.poster = image;
-              }})
-                res.json(movies);
-          }, (err) => next(err))
- }else{
-  err = new Error(' not found');
-  err.status = 404;
-}
+    .then(movies => {
+      movies.forEach((arrayItem)=>{
+        if(arrayItem.poster !=null){
+          var poster = arrayItem.poster
+          var posterLink = poster.split("/")
+          var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
+          arrayItem.poster = image
+        }})
+      if (movies.length === 0){
+          res.json("No more results ");
+       } else{
+         mongoose.connection.db
+         .collection("movieDetails")
+         .find(({title: new RegExp(req.query.title,'ig')}), { projection: { _id: 0, title:1, actors:1, plot:1, poster:1 }})
+         .count({})
+              .then(numberOfResult => {
+                  var pages = Math.ceil(numberOfResult/resPerPage)
+                  res.json({currentPage: page, result: numberOfResult, totalPage: pages, movies: movies});
+              });
+    }}, (err) => next(err))
+      .catch((err) => next(err)); 
+  }
 });
 
-movieRouter.get("/genre", authenticate.jwtCheck,  (req, res, next)=>{
+
+movieRouter.get("/genre", authenticate.jwtCheck, (req, res, next)=>{
   const resPerPage = 10; // results per page
-  const page = req.query.page || 1 // Page 
+  const page = parseInt(req.query.page) || 1 // Page 
   mongoose.connection.db               
   .collection("movieDetails")
-  .find(({genres: new RegExp(req.query.genre.toLowerCase(),'ig')}), {projection: {_id:0, title:1, imdb:1, genres:1, poster:1, actors:1, plot:1}})
+  .find(({genres: new RegExp(req.query.genre,'ig')}), {projection: {_id:0, title:1, imdb:1, genres:1, poster:1, actors:1, plot:1}})
   .sort({"imdb.rating":-1})
   .skip((resPerPage * page) - resPerPage)
   .limit(resPerPage)
   .toArray()
-  .then((movies) => {
+  .then(movies => {
     movies.forEach((arrayItem)=>{
       if(arrayItem.poster !=null){
         var poster = arrayItem.poster
         var posterLink = poster.split("/")
         var image = ("https://" + posterLink[2] + '/' + posterLink[3]+ '/' + posterLink[4] + '/' + posterLink[5])
-       arrayItem.poster = image;
-    }})
-      res.json(movies);
-  }, (err) => next(err))
-  .catch((err) => next(err)); 
+        arrayItem.poster = image
+      }})
+    if (movies.length === 0){
+        res.json("No more results ");
+     } else{
+       mongoose.connection.db
+       .collection("movieDetails")
+       .find(({genres: new RegExp(req.query.genre,'ig')}), {projection: {_id:0, title:1, imdb:1, genres:1, poster:1, actors:1, plot:1}})
+       .count({})
+            .then(numberOfResult => {
+                var pages = Math.ceil(numberOfResult/resPerPage)
+                res.json({currentPage: page, result: numberOfResult, totalPage: pages, movies: movies});
+            });
+  }}, (err) => next(err))
+    .catch((err) => next(err)); 
 });
 
 
-movieRouter.post('/update/:_id', authenticate.jwtCheck,  upload.none(), (req,res,next) => {
+movieRouter.post('/update/:_id',  upload.none(), (req,res,next) => {
+  if(req.body.actors){
+
   mongoose.connection.db
   .collection("movieDetails")
+  .findOne({_id : ObjectId(req.params._id)})
+  .then((movies)=>{
+    var actors1 = movies.actors
+        // console.log(actors1)
+    mongoose.connection.db
+    .collection("movieDetails")
+    .findOneAndUpdate({_id : ObjectId(req.params._id)}, { 
+      $addToSet: req.body
+    },{new: true},(err)=>{
+      if(err){
+        res.status(404)
+        res.send({
+          status: "Unsuccessful"
+        })
+        return
+      }
+      else if(actors1.indexOf(req.body.actors) === 1) {
+        res.send({status: "Already Exist"})
+        console.log(actors1)
+      }
+      else{
+        res.status(200)
+        res.send({
+            status: "Successful"
+          })
+          return
+      }
+      })
+  })
+}else{
+    mongoose.connection.db
+  .collection("movieDetails")
   .findOneAndUpdate({_id : ObjectId(req.params._id)}, { 
-
+    $set: req.body
   },{new: true},(err)=>{
     if(err){
       res.status(404)
@@ -251,10 +364,11 @@ movieRouter.post('/update/:_id', authenticate.jwtCheck,  upload.none(), (req,res
         return
     }
     })
+  }
 });
 
 
-movieRouter.delete("/delete/:_id", authenticate.jwtCheck,  (req, res, next)=>{
+movieRouter.delete("/delete/:_id",authenticate.jwtCheck,  (req, res, next)=>{
   mongoose.connection.db
   .collection("movieDetails")
   .findOneAndDelete({_id: ObjectId(req.params._id)},(err)=>{
@@ -273,7 +387,6 @@ movieRouter.delete("/delete/:_id", authenticate.jwtCheck,  (req, res, next)=>{
     }
     })
 });
-
 
 
 module.exports = movieRouter;
